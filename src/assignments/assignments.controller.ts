5bec1f3ff55ab84b9035cd9ad8e69b39b455aae1
sirc-cer <sirclaurence.cerrera@cvsu.edu.ph>
2024-11-09 22:54:23 +0800

    removed wrong placement of code

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -1,17 +1,5 @@
-import { Controller, Param, Get, ParseIntPipe } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 @Controller('assignments')
 export class AssignmentsController {
-    @Get('prime/:number')
-    isPrime(@Param('number', ParseIntPipe) number: number): { isPrime : boolean} {
-        const result = this.checkPrime(number);
-        return { isPrime: result };
-    }
-    private checkPrime(num: number): boolean {
-        if(num <= 1) return false;
-        for (let i=2; i <= Math.sqrt(num); i++){
-            if(num % i === 0) return false;
-        }
-        return true;
-    }
 }
