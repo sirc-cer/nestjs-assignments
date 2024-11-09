f87514f64ae9db6a4c0ace081f5b9864d7605766
sirc-cer <sirclaurence.cerrera@cvsu.edu.ph>
2024-11-09 22:33:33 +0800

    DONE assignment 2 prime

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Get, ParseIntPipe } from '@nestjs/common';
 
 @Controller('assignments')
-export class AssignmentsController {}
+export class AssignmentsController {
+    @Get('prime/:number')
+    isPrime(@Param('number', ParseIntPipe) number: number): { isPrime : boolean} {
+        const result = this.checkPrime(number);
+        return { isPrime: result };
+    }
+    private checkPrime(num: number): boolean {
+        if(num <= 1) return false;
+        for (let i=2; i <= Math.sqrt(num); i++){
+            if(num % i === 0) return false;
+        }
+        return true;
+    }
+}
