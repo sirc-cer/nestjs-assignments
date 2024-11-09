ec5e81754b388827b98268d650bbb10d8231b1d2
sirc-cer <sirclaurence.cerrera@cvsu.edu.ph>
2024-11-09 22:25:01 +0800

    done assingment 1 fibonacci

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('assignments')
-export class AssignmentsController {}
+export class AssignmentsController {
+    @Get('fibonacci/:n')
+    getFibonacci(@Param('n') n: string): number[]{
+        const num = parseInt(n);
+        if (isNaN(num) || num < 0){
+            throw new Error('Invalid number');
+            }
+            return this.calculateFibonacci(num);
+        }
+
+        private calculateFibonacci(n: number): number[]{
+            const fibonacci: number[] = [];
+            let a = 0, b =  1;
+
+            for (let i = 0; i < n; i++){
+                fibonacci.push(a);
+                const next = a + b;
+                a = b;
+                b = next;
+            }
+            return fibonacci;
+        }
+}
